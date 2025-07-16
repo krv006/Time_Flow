b40b3d68b5aaa219aacdb12aa2a292506f6f47f8
krv006 <rvkamronbek@gmail.com>
2025-07-16 17:21:28 +0500

    product

--- apps/views/product_view.py
+++ apps/views/product_view.py
@@ -2,6 +2,7 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from apps.models import Process, Product
+from apps.permission import IsManager
 from apps.serializers import ProcessModelSerializer
 from apps.serializers.product_serializer import ProductModelSerializer
 
@@ -9,10 +10,10 @@ from apps.serializers.product_serializer import ProductModelSerializer
 class ProductListCreateAPIView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductModelSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsManager)
 
 
 class ProcessListCreateAPIView(ListCreateAPIView):
     queryset = Process.objects.all()
     serializer_class = ProcessModelSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsManager)
