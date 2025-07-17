68c018097496a599d8431914dfc2a39b03d84598
krv006 <rvkamronbek@gmail.com>
2025-07-17 14:29:40 +0500

    manager create user model

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -2,3 +2,4 @@ from apps.models.managers import CustomUserManager
 from apps.models.product_model import Process, Product
 from apps.models.user_model import User
 from apps.models.managers import CustomUserManager
+from apps.models.shared_model import TimeBaseModel
--- apps/models/shared_model.py
+++ apps/models/shared_model.py
@@ -1,6 +1,6 @@
-from django.db.models import Model, DateTime
+from django.db.models import Model, DateTimeField
 
 
 class TimeBaseModel(Model):
-    created_at = DateTime(auto_now_add=True)
-    updated_at = DateTime(auto_now=True)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
